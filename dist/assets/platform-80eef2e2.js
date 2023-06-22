a517797f812ac448e466ed309ec715a09f201055
phuc.do <phuc.do@gss-sol.com>
2023-06-22 10:25:29 +0700

    refactor

--- dist/assets/platform-80eef2e2.js
+++
@@ -1,1 +0,0 @@
-function n(){return/iPhone/gi.test(window.navigator.platform)||/Mac/gi.test(window.navigator.platform)&&window.navigator.maxTouchPoints>0}function t(){return/Android/gi.test(window.navigator.userAgent)}function i(){return n()||t()}export{i as n,n as t};
